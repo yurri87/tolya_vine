bcfdaf898490a9cfedfece9ab5fc6ecec995f56a
Юрий <yurrizhulin@gmail.com>
2025-08-18 21:09:10 +0700

    fix: Remove legacy proxy from Vite config

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8788',
-        changeOrigin: true,
-      },
-    },
-  },
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
