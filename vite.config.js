f79d291b2e16b7316c9ac0e54f57e04649aeda18
Юрий <yurrizhulin@gmail.com>
2025-08-18 17:06:13 +0700

    feat: Prepare for Vercel migration

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8787',
+        target: 'http://localhost:8788',
         changeOrigin: true,
       },
     },
